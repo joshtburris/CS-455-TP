0de8abef37b3cab566bcf9dd954ddb2f2efcb3a7
Jack Fitzgerald <jhfitzg@rams.colostate.edu>
2020-04-23 20:50:47 -0600

    Switch to csv function to read files

--- PassengerAnalysis/src/main/java/PassengerAnalysis.java
+++ PassengerAnalysis/src/main/java/PassengerAnalysis.java
@@ -3,6 +3,7 @@ import scala.Tuple2;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.Row;
 
 import java.util.Arrays;
 import java.util.List;
@@ -10,7 +11,6 @@ import java.util.regex.Pattern;
 import java.lang.NumberFormatException;
 
 public final class PassengerAnalysis {
-  private static final Pattern COMMA = Pattern.compile(",");
 
   public static void main(String[] args) throws Exception {
 
@@ -19,39 +19,27 @@ public final class PassengerAnalysis {
       .appName("PassengerAnalysis")
       .getOrCreate();
 
-    //JavaRDD<String> lines = spark.read().textFile("/TP/1990.csv").javaRDD();
+    JavaRDD<Row> rows = spark.read().csv("/TP/1990.csv").javaRDD();
     
-    JavaRDD<String> lines = spark.read().textFile("/TP/1990.csv", "/TP/1991.csv", "/TP/1992.csv", "/TP/1993.csv", "/TP/1994.csv", "/TP/1995.csv",
+   /* JavaRDD<String> lines = spark.read().textFile("/TP/1990.csv", "/TP/1991.csv", "/TP/1992.csv", "/TP/1993.csv", "/TP/1994.csv", "/TP/1995.csv",
         "/TP/1996.csv", "/TP/1997.csv", "/TP/1998.csv", "/TP/1999.csv", "/TP/2000.csv", "/TP/2001.csv", "/TP/2002.csv", "/TP/2003.csv",
         "/TP/2004.csv", "/TP/2005.csv", "/TP/2006.csv", "/TP/2007.csv", "/TP/2008.csv", "/TP/2009.csv", "/TP/2010.csv", "/TP/2011.csv",
-        "/TP/2012.csv", "/TP/2013.csv", "/TP/2014.csv", "/TP/2015.csv", "/TP/2016.csv", "/TP/2017.csv", "/TP/2018.csv", "/TP/2019.csv").javaRDD();
+        "/TP/2012.csv", "/TP/2013.csv", "/TP/2014.csv", "/TP/2015.csv", "/TP/2016.csv", "/TP/2017.csv", "/TP/2018.csv", "/TP/2019.csv").javaRDD();*/
 
-    JavaRDD<List<String>> entries = lines.map(s -> Arrays.asList(COMMA.split(s)));   
-
-    JavaPairRDD<String, Double> passengersPerMonth = entries.mapToPair(s -> {
-        String year = PassengerAnalysis.findYear(s);
-        if (year != "") {
-            try {
-                int indexOfYear = s.indexOf(year);
-                int sizeOfList = s.size();
-                String month = s.get(sizeOfList - 3);
-                if (month.length() == 1) {
-                    month = "0" + month;
-                }
-                return new Tuple2<>(s.get(indexOfYear) + "-" + month, Double.parseDouble(s.get(0)));
+    JavaPairRDD<String, Double> airlinePassengersPerMonth = rows.mapToPair(row -> {
+        try {
+            String month = row.getString(33);
+            if (month.length() == 1) {
+                month = "0" + month;
             }
-            catch (NumberFormatException e) {
-                return new Tuple2<>("null", 0.0);
-            }
+            return new Tuple2<>(row.getString(31) + "-" + month, Double.parseDouble(row.getString(0)));
         }
-        else {
+        catch (NumberFormatException e) {
             return new Tuple2<>("null", 0.0);
         }
-    });
+    }).reduceByKey((i1, i2) -> i1 + i2).sortByKey();
 
-    JavaPairRDD<String, Double> sortedCounts = passengersPerMonth.reduceByKey((i1, i2) -> i1 + i2).sortByKey();
-
-    List<Tuple2<String, Double>> output = sortedCounts.collect();
+    List<Tuple2<String, Double>> output = airlinePassengersPerMonth.collect();
     for (Tuple2<?,?> tuple : output) {
       System.out.println(tuple._1() + "," + tuple._2());
     }
