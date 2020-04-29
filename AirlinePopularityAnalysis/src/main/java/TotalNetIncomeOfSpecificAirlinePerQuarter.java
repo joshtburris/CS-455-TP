4610f96965c266136ed9592587fff8bc51704ecc
Jack Fitzgerald <jhfitzg@rams.colostate.edu>
2020-04-29 02:45:07 -0600

    Added functionality to select a specific airline company to look at

--- AirlinePopularityAnalysis/src/main/java/TotalNetIncomeOfSpecificAirlinePerQuarter.java
+++ AirlinePopularityAnalysis/src/main/java/TotalNetIncomeOfSpecificAirlinePerQuarter.java
@@ -12,9 +12,11 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.File;
 
-public final class TotalNetIncomePerAirline {
+public final class TotalNetIncomeOfSpecificAirlinePerQuarter {
 
   public static void main(String[] args) throws Exception {
+    final String uniqueCarrierArgumentString = args[0];
+
     if (args.length < 1) {
       System.out.println("You must pass in at least 1 filepath");
       System.exit(1);
@@ -22,16 +24,21 @@ public final class TotalNetIncomePerAirline {
 
     SparkSession spark = SparkSession
       .builder()
-      .appName("TotalNetIncomePerAirline")
+      .appName("TotalNetIncomeOfSpecificAirlinePerQuarter")
       .getOrCreate();
     
-    JavaRDD<Row> rows = spark.read().csv(args).javaRDD();
+    JavaRDD<Row> rows = spark.read().csv(args[1]).javaRDD();
 
     JavaPairRDD<String, Double> totalNetIncomePerAirline = rows.mapToPair(row -> {
         try {
+            String year = row.getString(2);
+            String quarter = row.getString(3);
             String uniqueCarrierName = row.getString(1);
+            if (!uniqueCarrierName.equals(uniqueCarrierArgumentString)){
+                return new Tuple2<>("null", 0.0);
+            }
             Double netIncome = Double.parseDouble(row.getString(0));
-            return new Tuple2<>("\"" + uniqueCarrierName + "\"", netIncome);
+            return new Tuple2<>(year + "-" + quarter + ",\"" + uniqueCarrierName + "\"", netIncome);
         }
         catch (NumberFormatException e) {
             return new Tuple2<>("null", 0.0);
@@ -43,9 +50,9 @@ public final class TotalNetIncomePerAirline {
 
     //write net income per airline data to file
     List<Tuple2<String, Double>> netIncomeOutput = totalNetIncomePerAirline.collect();
-    File netIncomeOutputFile = new File("./total-net-income-per-airline.csv");
+    File netIncomeOutputFile = new File("./total-net-income-of-specific-airline-per-quarter.csv");
     BufferedWriter writer = new BufferedWriter(new FileWriter(netIncomeOutputFile));
-    writer.write("UNIQUE_CARRIER_NAME" + "," + "NET_INCOME\n");
+    writer.write("DATE" + "," + "UNIQUE_CARRIER_NAME" + "," + "NET_INCOME\n");
     for (Tuple2<String, Double> tuple : netIncomeOutput) {
       writer.write(tuple._1() + "," + tuple._2() + "\n");
     }
